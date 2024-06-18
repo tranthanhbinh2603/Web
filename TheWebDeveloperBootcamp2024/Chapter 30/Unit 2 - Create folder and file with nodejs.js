2dda4dd2e1bf94307e45da64790287e823572db5
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2024-06-18 22:26:20 +0700

    feat: add file in advancedcss

--- TheWebDeveloperBootcamp2024/Chapter 30/Unit 2 - Create folder and file with nodejs.js
+++ TheWebDeveloperBootcamp2024/Chapter 30/Unit 2 - Create folder and file with nodejs.js
@@ -1,13 +1,12 @@
-const fs = require('fs');
-const folderName = process.argv[2] || 'Project';
+const fs = require("fs");
+const folderName = process.argv[2] || "Project";
 
-try {    
-    fs.mkdirSync(folderName);
-    fs.writeFileSync(`${folderName}/index.html`, '');
-    fs.writeFileSync(`${folderName}/styles.css`, '');
-    fs.writeFileSync(`${folderName}/script.js`, '');
-}
-catch (e){
-    console.log('CREATE ERROR!!!!!')
-    console.error(e);
+try {
+	fs.mkdirSync(folderName);
+	fs.writeFileSync(`${folderName}/index.html`, "");
+	fs.writeFileSync(`${folderName}/styles.css`, "");
+	fs.writeFileSync(`${folderName}/script.js`, "");
+} catch (e) {
+	console.log("CREATE ERROR!!!!!");
+	console.error(e);
 }
