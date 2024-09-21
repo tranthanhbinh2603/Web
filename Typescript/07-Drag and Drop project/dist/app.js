38189aaa312cfeb11678d8db141afe1bb2bab135
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2024-09-21 22:31:28 +0700

    feat: [STEP 2]
    - Thêm 3 query selector cho các input element trong form đã tạo
    - Thực hiện thêm id form cho nó đẹp hơn (CSS có sẵn class rồi)
    - Thực hiện bắt sự kiện summit trong form
    - Thực hiện dùng hàm bind để truyền chính xác đối tượng cần bắt trong form

--- Typescript/07-Drag and Drop project/dist/app.js
+++ Typescript/07-Drag and Drop project/dist/app.js
@@ -3,9 +3,23 @@ class ProjectInput {
     constructor() {
         this.templateElement = document.querySelector("#project-input");
         this.hostElement = document.querySelector("#app");
+        this.titleInputElement = document.querySelector("#title");
+        this.descriptionInputElement = document.querySelector("#description");
+        this.peopleInputElement = document.querySelector("#people");
+        this.hostElement = document.querySelector("#app");
+        this.hostElement = document.querySelector("#app");
         const importNode = document.importNode(this.templateElement.content, true);
         this.formElement = importNode.firstElementChild;
+        this.formElement.id = "user-input";
+        this.configure();
         this.attach();
+    }
+    formHandler(e) {
+        e.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.formElement.addEventListener("submit", this.formHandler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
--- Typescript/07-Drag and Drop project/src/app.ts
+++ Typescript/07-Drag and Drop project/src/app.ts
@@ -2,15 +2,38 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	formElement: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.querySelector(
 			"#project-input"
 		)! as HTMLTemplateElement;
 		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
+		this.titleInputElement = document.querySelector(
+			"#title"
+		)! as HTMLInputElement;
+		this.descriptionInputElement = document.querySelector(
+			"#description"
+		)! as HTMLInputElement;
+		this.peopleInputElement = document.querySelector(
+			"#people"
+		)! as HTMLInputElement;
+		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
+		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
 		const importNode = document.importNode(this.templateElement.content, true);
 		this.formElement = importNode.firstElementChild as HTMLFormElement;
+		this.formElement.id = "user-input";
+		this.configure();
 		this.attach();
+	}
+	formHandler(e: Event) {
+		e.preventDefault();
+		console.log(this.titleInputElement.value);
+	}
+	configure() {
+		this.formElement.addEventListener("submit", this.formHandler.bind(this));
 	}
 	attach() {
 		this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
