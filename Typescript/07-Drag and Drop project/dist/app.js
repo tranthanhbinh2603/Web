adceb5cbf25e0d6da3de9f5cc9af375cdc668875
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2024-09-22 11:07:02 +0700

    feat: [STEP 3] them viec auto them ham bind cho class

--- Typescript/07-Drag and Drop project/dist/app.js
+++ Typescript/07-Drag and Drop project/dist/app.js
@@ -1,16 +1,33 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.querySelector("#project-input");
         this.hostElement = document.querySelector("#app");
-        this.titleInputElement = document.querySelector("#title");
-        this.descriptionInputElement = document.querySelector("#description");
-        this.peopleInputElement = document.querySelector("#people");
         this.hostElement = document.querySelector("#app");
         this.hostElement = document.querySelector("#app");
         const importNode = document.importNode(this.templateElement.content, true);
         this.formElement = importNode.firstElementChild;
         this.formElement.id = "user-input";
+        this.titleInputElement = this.formElement.querySelector("#title");
+        this.descriptionInputElement = this.formElement.querySelector("#description");
+        this.peopleInputElement = this.formElement.querySelector("#people");
         this.configure();
         this.attach();
     }
@@ -19,11 +36,14 @@ class ProjectInput {
         console.log(this.titleInputElement.value);
     }
     configure() {
-        this.formElement.addEventListener("submit", this.formHandler.bind(this));
+        this.formElement.addEventListener("submit", this.formHandler);
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
     }
 }
+__decorate([
+    autobind
+], ProjectInput.prototype, "formHandler", null);
 new ProjectInput();
 //# sourceMappingURL=app.js.map
--- Typescript/07-Drag and Drop project/src/app.ts
+++ Typescript/07-Drag and Drop project/src/app.ts
@@ -1,3 +1,17 @@
+function autobind(_: any, _2: string | symbol, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+
+	return adjDescriptor;
+}
+
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
@@ -11,29 +25,31 @@ class ProjectInput {
 			"#project-input"
 		)! as HTMLTemplateElement;
 		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
-		this.titleInputElement = document.querySelector(
-			"#title"
-		)! as HTMLInputElement;
-		this.descriptionInputElement = document.querySelector(
-			"#description"
-		)! as HTMLInputElement;
-		this.peopleInputElement = document.querySelector(
-			"#people"
-		)! as HTMLInputElement;
+
 		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
 		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
 		const importNode = document.importNode(this.templateElement.content, true);
 		this.formElement = importNode.firstElementChild as HTMLFormElement;
 		this.formElement.id = "user-input";
+		this.titleInputElement = this.formElement.querySelector(
+			"#title"
+		)! as HTMLInputElement;
+		this.descriptionInputElement = this.formElement.querySelector(
+			"#description"
+		)! as HTMLInputElement;
+		this.peopleInputElement = this.formElement.querySelector(
+			"#people"
+		)! as HTMLInputElement;
 		this.configure();
 		this.attach();
 	}
+	@autobind
 	formHandler(e: Event) {
 		e.preventDefault();
 		console.log(this.titleInputElement.value);
 	}
 	configure() {
-		this.formElement.addEventListener("submit", this.formHandler.bind(this));
+		this.formElement.addEventListener("submit", this.formHandler);
 	}
 	attach() {
 		this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
