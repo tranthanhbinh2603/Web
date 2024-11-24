70859325016971610fda610718b85c688f07ae0e
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2024-11-24 08:52:50 +0700

    feat: Learn Bun - Step 2
    - Tạo ra server backend đơn giản với Bun

--- Bun/01-web-server/index.ts
+++ Bun/01-web-server/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+	fetch(req) {
+		return new Response("Bun, haha!");
+	},
+});
+
+console.log(server.port); // 3000
+console.log(server.url); // http://localhost:3000
