6ef83dc3c0de0726df9db2689cc5ec34e88029e0
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2024-12-05 21:21:27 +0700

    feat: Microservice - Kubernetes - Step 7
    - Sửa lỗi file docker file để có thể ghi log chạy npm i (Lý do: chờ lâu quá)
    - Sửa đường link url kết nối giữa front-end và back-end
    - Tạo file infra tạo service và ip cluster cho client

--- Microservice/2_learn-docker-basic/c_kubernetes/client/src/PostCreate.js
+++ Microservice/2_learn-docker-basic/c_kubernetes/client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
 	const onSubmit = async (event) => {
 		event.preventDefault();
 
-		await axios.post("http://post-srv:5050/posts", {
+		await axios.post("http://posts.com/posts", {
 			title,
 		});
 
--- Microservice/2_learn-docker-basic/c_kubernetes/client/src/PostList.js
+++ Microservice/2_learn-docker-basic/c_kubernetes/client/src/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
 	const [posts, setPosts] = useState({});
 
 	const fetchPosts = async () => {
-		const res = await axios.get("http://query-srv:5099/posts");
+		const res = await axios.get("http://posts.com/posts");
 
 		setPosts(res.data);
 	};
