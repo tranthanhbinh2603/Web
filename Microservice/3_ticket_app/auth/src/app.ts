f0d3626fbbd373d700863263d8c2f3fe3e3b1e97
Trần Thanh Bình <tranthanhbinh.thcsthanhson@gmail.com>
2024-12-16 20:44:51 +0700

    feat: Microservice - Ticket App - Step 15.5
    - Xoá bớt báo lỗi ra ngoài do lỗi thiếu miss JWT_KEY
    - Bật extended urlencoded của Express

--- Microservice/3_ticket_app/auth/src/app.ts
+++ Microservice/3_ticket_app/auth/src/app.ts
@@ -16,7 +16,7 @@ app.use(
 	})
 );
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use(helmet({ contentSecurityPolicy: false }));
 
